aaa736b03672c361df70979889157ac2e0600182
qwerhnatkiv <qwerhnatkiv@gmail.com>
2024-08-05 02:58:29 +0300

    add time logging

---
+++ FantasyWeb.Services/Interceptors/MethodTimeLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace FantasyWeb.Services.Interceptors
+{
+    public static class MethodTimeLogger
+    {
+        public static void Log(MethodBase methodBase, long milliseconds, string message)
+        {
+            const string fileName = "timer.log";
+            using (StreamWriter streamWriter = File.AppendText(fileName))
+            {
+                streamWriter.WriteLine(methodBase.Name + " " + milliseconds + "ms");
+            }
+        }
+    }
+}
