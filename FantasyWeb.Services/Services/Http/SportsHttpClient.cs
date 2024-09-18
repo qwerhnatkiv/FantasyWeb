fa6afd53fbdde4e4d5d614e622fb021b3568b9f8
qwerhnatkiv <qwerhnatkiv@gmail.com>
2024-09-19 00:17:33 +0300

    set dummy data for any sports player info

--- FantasyWeb.Services/Services/Http/SportsHttpClient.cs
+++ FantasyWeb.Services/Services/Http/SportsHttpClient.cs
@@ -9,21 +9,49 @@ namespace FantasyWeb.Services.Services.Http
     {
         public async Task<SportsSquadDTO> GetPlayersByAccountId(string accountId)
         {
-            SportsSquadDTO sportsSquadDTO = await $"https://www.sports.ru/fantasy/hockey/team/json/{accountId}.json"
-                .GetJsonAsync<SportsSquadDTO>();
+            //SportsSquadDTO sportsSquadDTO = await $"https://www.sports.ru/fantasy/hockey/team/json/{accountId}.json"
+            //    .GetJsonAsync<SportsSquadDTO>();
 
-            // Get Balance
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument? sportsRuSquadPage = web.Load($"https://www.sports.ru/fantasy/hockey/team/{accountId}.html");
-            HtmlNode? balanceLabelNode = sportsRuSquadPage.DocumentNode.SelectSingleNode("//*[text() = 'Баланс']");
-            HtmlNode? balanceValueNode = balanceLabelNode.ParentNode.SelectSingleNode("td");
-            sportsSquadDTO.Balance = int.Parse(balanceValueNode.InnerText);
+            //// Get Balance
+            //HtmlWeb web = new HtmlWeb();
+            //HtmlDocument? sportsRuSquadPage = web.Load($"https://www.sports.ru/fantasy/hockey/team/{accountId}.html");
+            //HtmlNode? balanceLabelNode = sportsRuSquadPage.DocumentNode.SelectSingleNode("//*[text() = 'Баланс']");
+            //HtmlNode? balanceValueNode = balanceLabelNode.ParentNode.SelectSingleNode("td");
+            //sportsSquadDTO.Balance = int.Parse(balanceValueNode.InnerText);
 
-            HtmlNode? substitutionsLabelNode = sportsRuSquadPage.DocumentNode.SelectSingleNode("//th[text() = 'Трансферы']");
-            HtmlNode? substitutionsValueNode = substitutionsLabelNode.ParentNode.SelectSingleNode("td");
-            sportsSquadDTO.Substitutions = int.Parse(substitutionsValueNode.InnerText);
+            //HtmlNode? substitutionsLabelNode = sportsRuSquadPage.DocumentNode.SelectSingleNode("//th[text() = 'Трансферы']");
+            //HtmlNode? substitutionsValueNode = substitutionsLabelNode.ParentNode.SelectSingleNode("td");
+            //sportsSquadDTO.Substitutions = int.Parse(substitutionsValueNode.InnerText);
 
-            return sportsSquadDTO;
+            //return sportsSquadDTO;
+
+            return new SportsSquadDTO()
+            {
+                Balance = 20000,
+                Substitutions = 5,
+                Players = new List<SportsSquadPlayerDTO>
+                {
+                    new SportsSquadPlayerDTO () { Id = 1087 },
+                    new SportsSquadPlayerDTO () { Id = 283 },
+
+                    new SportsSquadPlayerDTO () { Id = 586 },
+                    new SportsSquadPlayerDTO () { Id = 1118 },
+                    new SportsSquadPlayerDTO () { Id = 1035 },
+                    new SportsSquadPlayerDTO () { Id = 391 },
+                    new SportsSquadPlayerDTO () { Id = 140 },
+                    new SportsSquadPlayerDTO () { Id = 1165 },
+
+                    new SportsSquadPlayerDTO () { Id = 592 },
+                    new SportsSquadPlayerDTO () { Id = 600 },
+                    new SportsSquadPlayerDTO () { Id = 502 },
+                    new SportsSquadPlayerDTO () { Id = 673 },
+                    new SportsSquadPlayerDTO () { Id = 735 },
+                    new SportsSquadPlayerDTO () { Id = 1183 },
+                    new SportsSquadPlayerDTO () { Id = 719 },
+                    new SportsSquadPlayerDTO () { Id = 606 },
+                    new SportsSquadPlayerDTO () { Id = 734 },
+                }
+            };
         }
     }
 }
